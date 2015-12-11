e3ccfc7e500baf5277e878a502ed98f1dd31cdcb
isaac-you <issac.you@qq.com>
2015-12-11 14:43:02 +0800

    Create demo.m

---
+++ demo.m
@@ -0,0 +1,16 @@
+% demonstration on how to use IsoFdp.
+% 
+% Note: we cannot be responsible for its use, misuse, 
+% or functionality. 
+%
+
+
+clear
+close all
+resultnmi(8)=0;
+
+for i=1:8
+	[~,~,resultnmi(i),~,~,~,~,~]=IsoFdp(sprintf('LFR_data\\network_mix%d.dat',i),10,30,'structure',15,50,sprintf('LFR_data\\community_mix%d.dat',i),'PD2',1);
+end
+
+save resultnmi.mat resultnmi
